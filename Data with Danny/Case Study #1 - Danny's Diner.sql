2f0bb74ac59f55aa53a17c2b2eea921786ef3f42
durkka25 <280074141+durkka25@users.noreply.github.com>
2026-07-08 16:37:45 +0100

    Update Case Study #1 - Danny's Diner.sql

--- Data with Danny/Case Study #1 - Danny's Diner.sql
+++ Data with Danny/Case Study #1 - Danny's Diner.sql
@@ -5,17 +5,51 @@ select customer_id
     ,sum(price) as total_spend
 from sales 
 join menu on sales.product_id=menu.product_id
-group by 1
+group by 1;
 
 --2. How many days has each customer visited the restaurant?
 select customer_id
     ,count (distinct order_date) as days_visited
 from sales 
-group by 1
+group by 1;
 
 --3. What was the first item from the menu purchased by each customer?
+with cte as (
+    select customer_id
+        ,order_date
+        ,product_name
+        ,dense_rank() over(
+            partition by customer_id
+            order by order_date asc
+        ) as order_sequence
+    from sales
+    join menu on sales.product_id=menu.product_id
+)
+
+select customer_id
+    ,product_name as first_item_purchased
+from cte
+where order_sequence=1;
+
 --4. What is the most purchased item on the menu and how many times was it purchased by all customers?
 --5. Which item was the most popular for each customer?
+with cte as (
+    select customer_id 
+        ,product_name 
+        ,count(product_name)
+        ,dense_rank() over(
+            partition by customer_id 
+            order by count(product_name) desc) as popularity
+    from sales 
+    join menu on sales.product_id = menu.product_id
+    group by customer_id
+        ,product_name
+)
+
+select customer_id 
+    ,product_name as most_popular_product
+from cte
+where popularity=1;
 --6. Which item was purchased first by the customer after they became a member?
 --7. Which item was purchased just before the customer became a member?
 --8. What is the total items and amount spent for each member before they became a member?
